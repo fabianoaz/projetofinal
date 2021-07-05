90fbee5fd3956c587fa5b138f4cb23494a296a0e
Fabiano Azevedo <fabianoaz@hotmail.com>
2021-07-05 12:06:09 -0300

    feat: Teste deploy Heroku

--- server.js
+++ server.js
@@ -7,9 +7,19 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./app'));
 
+/*
 app.get('/*', (req, res) =>
     res.sendFile('app.component.html', {root: 'app/'}),
 );
+*/
+
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/src/app'));
+console.log("TESTE CARREGAR: " + __dirname)
+
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/src/app/app.component.html'));   
+});  
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
